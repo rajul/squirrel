c5ba9ab77a2dd84133e9f66045cf2a4445408426
Rajul <rajul09@gmail.com>
2016-03-08 00:29:50 +0700

    Added LRUCacheDict class

---
+++ squirrel/LRUCacheDict.py
@@ -0,0 +1,27 @@
+from squirrel.BaseCache import BaseCache
+
+class LRUCacheDict(BaseCache):
+	def __init__(self, capacity=100):
+		super(LRUCacheDict, self).__init__(capacity)
+
+		self.cache = {}
+		self.lru = {}
+		self.access_time = 0
+
+	def get(self, key):
+		if key in self.cache:
+			self.lru[key] = self.access_time
+			self.access_time = self.access_time + 1
+			return self.cache[key]
+
+		return -1
+
+	def set(self, key, value):
+		if len(self.cache) >= self.capacity:
+			lru_key = min(self.lru.keys(), key=lambda k: self.lru[k])
+			self.cache.pop(lru_key)
+			self.lru.pop(lru_key)
+
+		self.cache[key] = value
+		self.lru[key] = self.access_time
+		self.access_time = self.access_time + 1
